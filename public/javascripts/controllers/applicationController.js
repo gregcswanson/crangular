08499bbc44c6c0d36307916d193c7066e3376f3f
Gregory Swanson <gregcswanson@gmail.com>
2013-01-28 04:57:53 -0500

    menu controller and partial

--- public/javascripts/controllers/applicationController.js
+++ public/javascripts/controllers/applicationController.js
@@ -8,3 +8,16 @@ function ApplicationCtrl($scope) {
 function HomeCtrl($scope) {
     $scope.page = "Home";
 }
+
+function MenuCtrl($scope) {
+    $scope.isExpanded = true;
+    $scope.label = "hide menu";
+    $scope.expandedClick = function(){
+        $scope.isExpanded = !$scope.isExpanded;
+        if($scope.isExpanded){
+            $scope.label = "hide menu";
+        }else{
+            $scope.label = "show menu";
+        }
+    };
+}
