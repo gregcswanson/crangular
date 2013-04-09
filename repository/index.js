436b4366f6245083697f8a9901890000178cf84a
Gregory Swanson <gregcswanson@gmail.com>
2013-04-09 10:50:11 +1000

    mongo connection configuration
    handle undefined connection parameters

--- repository/index.js
+++ repository/index.js
@@ -1,57 +1,68 @@
 /* 
-    Use MONGODB as the repository for all data
-    This module is only concerned with CRUD operations and not with any validations
+Use MONGODB as the repository for all data
+This module is only concerned with CRUD operations and not with any validations
 */
 
 var Db = require('mongodb').Db
  , Server = require('mongodb').Server
  , ObjectID = require('mongodb').ObjectID
  , nconf = require('nconf');
- 
- console.log(nconf.get('MONGO_DB_SERVER'));
 
-var server = new Server(nconf.get('MONGO_DB_SERVER').toString(), nconf.get('MONGO_DB_PORT').toString(), { auto_reconnect: true });
-var db = new Db(nconf.get('MONGO_DB_DATABASE').toString(),server, {safe:false});
+var db;
+console.log(nconf.get('MONGO_DB_SERVER'));
 
-db.open(function(err) {
-    if(nconf.get('MONGO_DB_USERNAME').toString() !== "")
-    {
-        db.authenticate(
-            nconf.get('MONGO_DB_USERNAME').toString(), 
-            nconf.get('MONGO_DB_PASSWORD').toString(), 
-            function(err) {
+var mongoServer = nconf.get('MONGO_DB_SERVER') || '';
+var mongoPort = nconf.get('MONGO_DB_PORT') || '';
+var mongoDatabase = nconf.get('MONGO_DB_DATABASE') || '';
+var mongoUsername = nconf.get('MONGO_DB_USERNAME') || '';
+var mongoPassword = nconf.get('MONGO_DB_PASSWORD') || '';
+
+if (mongoServer === '') {
+    console.log('connection settings to mongoDB have not been defined');
+} else {
+
+    var server = new Server(mongoServer, mongoPort, { auto_reconnect: true });
+    db = new Db(mongoDatabase, server, { safe: false });
+
+    db.open(function (err) {
+        if (mongoUsername !== "") {
+            db.authenticate(
+            mongoUsername,
+            mongoPassword,
+            function (err) {
                 if (err) {
                     console.log(err);
-                }else{
-                    console.log('connected to mongoDB (' + nconf.get('MONGO_DB_SERVER').toString() + ', ' + nconf.get('MONGO_DB_DATABASE').toString() + ')');
+                } else {
+                    console.log('connected to mongoDB (' + mongoServer + ', ' + mongoDatabase + ')');
                 }
             }
         );
-    }
-});
+        }
+    });
+}
 
-exports.VentsFindAll = function(callback) {
-    db.collection('vents', function(err, collection) {
-        collection.find({}).sort([['created', 'desc']]).toArray(function(err, items) {
-            if(err) return callback(err, null);
+exports.VentsFindAll = function (callback) {
+    db.collection('vents', function (err, collection) {
+        collection.find({}).sort([['created', 'desc']]).toArray(function (err, items) {
+            if (err) return callback(err, null);
             callback(null, items);
         });
     });
 };
 
-exports.VentsAdd = function(item, callback) {
-    db.collection('vents', function(err, collection) {
-        collection.insert(item, {safe:true}, function(err, result) {
-            if(err) return callback(err, null);
+exports.VentsAdd = function (item, callback) {
+    db.collection('vents', function (err, collection) {
+        collection.insert(item, { safe: true }, function (err, result) {
+            if (err) return callback(err, null);
             callback(null, result);
         });
     });
 };
 
-exports.ProfileFindByID = function(id, callback) {
-	db.collection('profiles', function(err, collection) {
-        collection.find({_id: new ObjectID(id)},{limit: 1}).toArray(function(err, docs){ 
-            if(err) return callback(err, null);
+exports.ProfileFindByID = function (id, callback) {
+    db.collection('profiles', function (err, collection) {
+        collection.find({ _id: new ObjectID(id) }, { limit: 1 }).toArray(function (err, docs) {
+            if (err) return callback(err, null);
             if (docs.length > 0) {
                 callback(null, docs[0]);
             } else {
@@ -61,38 +72,38 @@ exports.ProfileFindByID = function(id, callback) {
     });
 };
 
-exports.ProfileFindAll = function(callback) {
-    db.collection('profiles', function(err, collection) {
+exports.ProfileFindAll = function (callback) {
+    db.collection('profiles', function (err, collection) {
         //collection.find({}).sort([['created', 'desc']]).toArray(function(err, docs) {
-        collection.find({}).toArray(function(err, items) {
-            if(err) return callback(err, null);
+        collection.find({}).toArray(function (err, items) {
+            if (err) return callback(err, null);
             callback(null, items);
         });
     });
 };
 
-exports.ProfileAdd = function(item, callback) {
-    db.collection('profiles', function(err, collection) {
-        collection.insert(item, {safe:true}, function(err, result) {
-            if(err) return callback(err, null);
-			callback(null, result);
+exports.ProfileAdd = function (item, callback) {
+    db.collection('profiles', function (err, collection) {
+        collection.insert(item, { safe: true }, function (err, result) {
+            if (err) return callback(err, null);
+            callback(null, result);
         });
     });
 };
 
-exports.ProfileUpdate = function(blog, callback) {
-    db.collection('profiles', function(err, collection) {
-        collection.findAndModify({_id: new ObjectID(blog._id)}, {}, {$set: {comment: blog.comment}}, {} , function(err, item) {
-            if(err) return callback(err, null);
+exports.ProfileUpdate = function (blog, callback) {
+    db.collection('profiles', function (err, collection) {
+        collection.findAndModify({ _id: new ObjectID(blog._id) }, {}, { $set: { comment: blog.comment} }, {}, function (err, item) {
+            if (err) return callback(err, null);
             callback(null, item);
         });
     });
 };
 
-exports.ProfileDelete = function(id, callback) {
-    db.collection('profiles', function(err, collection) {
-        collection.findAndModify({_id: new ObjectID(id)}, {}, {}, { remove: true } , function(err, item) {
-            if(err) return callback(err, null);
+exports.ProfileDelete = function (id, callback) {
+    db.collection('profiles', function (err, collection) {
+        collection.findAndModify({ _id: new ObjectID(id) }, {}, {}, { remove: true }, function (err, item) {
+            if (err) return callback(err, null);
             console.log(item);
             callback(null, item);
         });
--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,45 @@
 var nconf = require('nconf');
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function (req, res) {
+    res.render('index', { title: 'Express' });
 };
 
-exports.configuration = function(req, res){
-    if(!req.session.configurationConfirmed || !req.session.configurationConfirmed){
+exports.configuration = function (req, res) {
+    if (!req.session.configurationConfirmed || !req.session.configurationConfirmed) {
         res.render('configurationLogin', { title: 'Configuration' });
-    }else{
+    } else {
         var temp = req.session.temp;
         if (!temp) temp = "";
-        res.render('configuration', 
-        { 
-            title: 'Configuration', 
-            secret: nconf.get('http:secret'), 
+        res.render('configuration',
+        {
+            title: 'Configuration',
+            secret: nconf.get('http:secret'),
             password: '',
-            name: nconf.get('AZURE_STORAGE_ACCOUNT'),
-            key: nconf.get('AZURE_STORAGE_ACCESS_KEY'),
-            mongodbserver: nconf.get('MONGO_DB_SERVER'),
-            mongodbport: nconf.get('MONGO_DB_PORT'),
-            mongodbdatabase: nconf.get('MONGO_DB_DATABASE'),
-            mongodbusername: nconf.get('MONGO_DB_USERNAME'),
-            mongodbpassword: nconf.get('MONGO_DB_PASSWORD')
-        });  
+            name: nconf.get('AZURE_STORAGE_ACCOUNT') || '',
+            key: nconf.get('AZURE_STORAGE_ACCESS_KEY') || '',
+            mongodbserver: nconf.get('MONGO_DB_SERVER') || '',
+            mongodbport: nconf.get('MONGO_DB_PORT') || '',
+            mongodbdatabase: nconf.get('MONGO_DB_DATABASE') || '',
+            mongodbusername: nconf.get('MONGO_DB_USERNAME') || '',
+            mongodbpassword: nconf.get('MONGO_DB_PASSWORD') || ''
+        });
     }
 };
 
-exports.configurationLogin = function(req, res){
-    if(nconf.get('password') == req.body.password)
-    {
+exports.configurationLogin = function (req, res) {
+    if (nconf.get('password') == req.body.password) {
         req.session.configurationConfirmed = true;
     }
     res.redirect('/configuration');
 };
 
-exports.congifurationLogout = function(req, res){
+exports.congifurationLogout = function (req, res) {
     req.session.configurationConfirmed = false;
     res.redirect('/');
 };
 
-exports.configurationPost = function(req, res){
-    if(req.body.password && req.body.password.length !== 0){
+exports.configurationPost = function (req, res) {
+    if (req.body.password && req.body.password.length !== 0) {
         nconf.set('password', req.body.password);
     }
     nconf.set('http:secret', req.body.secret);
@@ -51,7 +50,7 @@ exports.configurationPost = function(req, res){
     nconf.set('MONGO_DB_DATABASE', req.body.mongodbdatabase);
     nconf.set('MONGO_DB_USERNAME', req.body.mongodbusername);
     nconf.set('MONGO_DB_PASSWORD', req.body.mongodbpassword);
-    nconf.save(function(){
+    nconf.save(function () {
         res.redirect('/configuration');
     });
 };
